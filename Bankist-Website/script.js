67c4c0360158e52f5aa98c5906fea8068c945b33
Damian Pietrewicz <d.pietrewicz4@gmail.com>
2024-03-05 20:44:22 +0100

    192.done

--- Bankist-Website/script.js
+++ Bankist-Website/script.js
@@ -164,3 +164,30 @@ setTimeout(() => h1.removeEventListener('mouseenter', alertH1), 3000); // option
 // };
 
 // 192. -----------------------------------------------------------------
+// rgb(255,255,255)
+const randomInt = (min, max) =>
+  Math.floor(Math.random() * (max - min + 1) + min);
+const randomColor = () =>
+  `rgb(${randomInt(0, 255)},${randomInt(0, 255)},${randomInt(0, 255)})`;
+console.log(randomColor());
+
+document.querySelector('.nav__link').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+  console.log('LINK', e.target, e.currentTarget);
+  console.log(e.currentTarget === this); // important!
+
+  // Stop propagation
+  // e.stopPropagation();
+});
+
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  console.log('CONTAINER', e.target, e.currentTarget);
+  this.style.backgroundColor = randomColor();
+});
+
+document.querySelector('.nav').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+  console.log('NAV', e.target, e.currentTarget);
+});
+
+// 193. -----------------------------------------------------------------
