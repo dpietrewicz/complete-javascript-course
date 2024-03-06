159c1e7b3983f87df69287f4635fb70c27083fa2
Damian Pietrewicz <d.pietrewicz4@gmail.com>
2024-03-06 13:10:41 +0100

    194.done

--- Bankist-Website/script.js
+++ Bankist-Website/script.js
@@ -7,6 +7,8 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
 
 const openModal = function (e) {
   e.preventDefault();
@@ -30,96 +32,8 @@ document.addEventListener('keydown', function (e) {
   }
 });
 
-// 187. Selecting, Creating, and Deleting Elements ---------------
-
-// console.log(document.documentElement);
-// console.log(document.head);
-// console.log(document.body);
-
-const header = document.querySelector('.header');
-const allSections = document.querySelectorAll('.section');
-// console.log(allSections);
-
-document.getElementById('section--1');
-const allButtons = document.getElementsByTagName('button');
-// console.log(allButtons);
-
-// console.log(document.getElementsByClassName('btn'));
-
-// Creating and inserting  elements
-// .insertAdjacentHTML;
-
-const message = document.createElement('div');
-message.classList.add('cookie-message');
-message.textContent = 'We use cookies for improved functionality and analytics';
-message.innerHTML =
-  'We use cookies for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>';
-
-// header.prepend(message);
-header.append(message);
-// header.append(message.cloneNode(true));
-
-// header.before(message);
-// header.before(message);
-
-// Delete elements
-document
-  .querySelector('.btn--close-cookie')
-  .addEventListener('click', function () {
-    message.remove();
-    // message.parentElement.removeChild(message); old way
-  });
-
-// 188. -----------------------------------------------------------------
-// Styles
-message.style.backgroundColor = '#37383d';
-message.style.width = '120%';
-
-// console.log(message.style.color);
-// console.log(message.style.backgroundColor);
-
-// console.log(getComputedStyle(message).color);
-// console.log(getComputedStyle(message).height);
-
-message.style.height =
-  Number.parseFloat(getComputedStyle(message).height, 10) + 30 + 'px';
-
-// document.documentElement.style.setProperty('--color-primary', 'orangered');
-
-// Attributes
-const logo = document.querySelector('.nav__logo');
-// console.log(logo.alt);
-// console.log(logo.className);
-
-// logo.alt = 'Beautiful minimalist logo';
-
-// Non-standard
-// console.log(logo.designer); // undefined
-// console.log(logo.getAttribute('designer')); // Jonas
-// logo.setAttribute('company', 'Bankist');
-
-// console.log(logo.src); // http://127.0.0.1:5500/Bankist-Website/img/logo.png
-// console.log(logo.getAttribute('src')); // img/logo.png
-
-const link = document.querySelector('.nav__link--btn');
-// console.log(link.href);
-// console.log(link.getAttribute('href'));
-
-// Data attributes
-// console.log(logo.dataset.versionNumber);
-
-// Classes
-logo.classList.add('a', 'j');
-logo.classList.remove('a');
-logo.classList.toggle('a');
-logo.classList.contains('a');
-
-// Don't use because this will overrde all the existing classes
-// logo.className = 'jonas';
-
 // 189. -----------------------------------------------------------------
-const btnScrollTo = document.querySelector('.btn--scroll-to');
-const section1 = document.querySelector('#section--1');
+// Button scrolling
 btnScrollTo.addEventListener('click', function (e) {
   const s1coords = section1.getBoundingClientRect();
   // console.log(s1coords);
@@ -144,32 +58,92 @@ btnScrollTo.addEventListener('click', function (e) {
   section1.scrollIntoView({ behavior: 'smooth' });
 });
 
-// 190. -----------------------------------------------------------------
+// 193. -----------------------------------------------------------------
+// Page navigation
 
-const h1 = document.querySelector('h1');
+// document.querySelectorAll('.nav__link').forEach(function (el) {
+//   el.addEventListener('click', function (e) {
+//     e.preventDefault();
+//     const id = this.getAttribute('href');
+//     console.log(id);
+//     document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+//   });
+// });
 
-const alertH1 = function (e) {
-  alert('addEventListener: Great! You are reading the heading :D');
+// EVENT DELEGATION
+// 1. Add event listener to common parent element
+// 2. Determine what element originated the event
 
-  // h1.removeEventListener('mouseenter', alertH1);  // option I
-};
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  console.log(e.target);
+  e.preventDefault();
+  // Matching strategy
+  if (e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  }
+});
 
-h1.addEventListener('mouseenter', alertH1);
+// 195. -----------------------------------------------------------------
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
 
-setTimeout(() => h1.removeEventListener('mouseenter', alertH1), 3000); // option II
+// Not useful
+// tabs.forEach(t => t.addEventListener('click', () => console.log('TAB')));
 
-// old-school
-// h1.onmouseenter = function (e) {
-//   alert('addEventListener: Great! You are reading the heading :D');
-// };
+tabsContainer.addEventListener('click', function (e) {
+  const clicked = e.target.parentElement;
+  console.log(clicked);
+});
+
+//
+//
+// ------------------------------------------------------------------- //
+//                             LESSONS
+// ------------------------------------------------------------------- //
+
+// 194. -----------------------------------------------------------------
+// DOM Traversing
+// const h1 = document.querySelector('h1');
+
+// Going downwards: child
+// NOTES:
+// 1. it will be selected no matter how deep the child is inside the parent el. h1
+// 2. other elements with class '.highlight' - they would not be selected as they are not children of h1 element
+// console.log(h1.querySelectorAll('.highlight'));
+// console.log(h1.childNodes);
+// console.log(h1.children);
+// h1.firstElementChild.style.color = 'white';
+// h1.lastElementChild.style.color = 'orangered';
+
+// Going upwards: parents
+// console.log(h1.parentNode);
+// console.log(h1.parentElement);
+
+// h1.closest('.header').style.background = 'var(--gradient-secondary)';
+// h1.closest('h1').style.background = 'var(--gradient-primary)';
+
+// Going sideways: siblings
+// console.log(h1.previousElementSibling);
+// console.log(h1.nextElementSibling);
+
+// console.log(h1.previousSibling);
+// console.log(h1.nextSibling);
+
+// console.log(h1.parentElement.children);
+// [...h1.parentElement.children].forEach(function (el) {
+//   if (el !== h1) el.style.transform = 'scale(0.5)';
+// });
 
 // 192. -----------------------------------------------------------------
 // rgb(255,255,255)
+/*
 const randomInt = (min, max) =>
   Math.floor(Math.random() * (max - min + 1) + min);
 const randomColor = () =>
   `rgb(${randomInt(0, 255)},${randomInt(0, 255)},${randomInt(0, 255)})`;
-console.log(randomColor());
+// console.log(randomColor());
 
 document.querySelector('.nav__link').addEventListener('click', function (e) {
   this.style.backgroundColor = randomColor();
@@ -189,5 +163,113 @@ document.querySelector('.nav').addEventListener('click', function (e) {
   this.style.backgroundColor = randomColor();
   console.log('NAV', e.target, e.currentTarget);
 });
+*/
 
-// 193. -----------------------------------------------------------------
+// 190. -----------------------------------------------------------------
+/*
+const h1 = document.querySelector('h1');
+
+const alertH1 = function (e) {
+  alert('addEventListener: Great! You are reading the heading :D');
+
+  // h1.removeEventListener('mouseenter', alertH1);  // option I
+};
+
+h1.addEventListener('mouseenter', alertH1);
+
+setTimeout(() => h1.removeEventListener('mouseenter', alertH1), 3000); // option II
+*/
+// old-school
+// h1.onmouseenter = function (e) {
+//   alert('addEventListener: Great! You are reading the heading :D');
+// };
+
+// 187. Selecting, Creating, and Deleting Elements ---------------
+
+// console.log(document.documentElement);
+// console.log(document.head);
+// console.log(document.body);
+
+// const header = document.querySelector('.header');
+// const allSections = document.querySelectorAll('.section');
+// console.log(allSections);
+
+// document.getElementById('section--1');
+// const allButtons = document.getElementsByTagName('button');
+// console.log(allButtons);
+
+// console.log(document.getElementsByClassName('btn'));
+
+// Creating and inserting  elements
+// .insertAdjacentHTML;
+/*
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+message.textContent = 'We use cookies for improved functionality and analytics';
+message.innerHTML =
+  'We use cookies for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>';
+*/
+// header.prepend(message);
+// header.append(message);
+// header.append(message.cloneNode(true));
+
+// header.before(message);
+// header.before(message);
+
+// Delete elements
+/*
+document
+  .querySelector('.btn--close-cookie')
+  .addEventListener('click', function () {
+    message.remove();
+    // message.parentElement.removeChild(message); old way
+  });
+*/
+// 188. -----------------------------------------------------------------
+// Styles
+
+// message.style.backgroundColor = '#37383d';
+// message.style.width = '120%';
+
+// console.log(message.style.color);
+// console.log(message.style.backgroundColor);
+
+// console.log(getComputedStyle(message).color);
+// console.log(getComputedStyle(message).height);
+
+// message.style.height =
+//   Number.parseFloat(getComputedStyle(message).height, 10) + 30 + 'px';
+
+// document.documentElement.style.setProperty('--color-primary', 'orangered');
+
+// Attributes
+// const logo = document.querySelector('.nav__logo');
+// console.log(logo.alt);
+// console.log(logo.className);
+
+// logo.alt = 'Beautiful minimalist logo';
+
+// Non-standard
+// console.log(logo.designer); // undefined
+// console.log(logo.getAttribute('designer')); // Jonas
+// logo.setAttribute('company', 'Bankist');
+
+// console.log(logo.src); // http://127.0.0.1:5500/Bankist-Website/img/logo.png
+// console.log(logo.getAttribute('src')); // img/logo.png
+
+// const link = document.querySelector('.nav__link--btn');
+// console.log(link.href);
+// console.log(link.getAttribute('href'));
+
+// Data attributes
+// console.log(logo.dataset.versionNumber);
+
+// Classes
+/*
+logo.classList.add('a', 'j');
+logo.classList.remove('a');
+logo.classList.toggle('a');
+logo.classList.contains('a');
+*/
+// Don't use because this will overrde all the existing classes
+// logo.className = 'jonas';
