a3b001a465131ed80f6474da01e9de992dae6d78
Damian Pietrewicz <d.pietrewicz4@gmail.com>
2024-03-07 12:50:56 +0100

    196.done

--- Bankist-Website/script.js
+++ Bankist-Website/script.js
@@ -93,8 +93,24 @@ const tabsContent = document.querySelectorAll('.operations__content');
 // tabs.forEach(t => t.addEventListener('click', () => console.log('TAB')));
 
 tabsContainer.addEventListener('click', function (e) {
-  const clicked = e.target.parentElement;
+  const clicked = e.target.closest('.operations__tab');
   console.log(clicked);
+
+  // Guard clause
+  if (!clicked) return;
+
+  // Remove active classes
+  tabs.forEach(t => t.classList.remove('operations__tab--active'));
+  tabsContent.forEach(c => c.classList.remove('operations__content--active'));
+
+  // Activate tab
+  clicked.classList.add('operations__tab--active');
+
+  // Activate content area
+  console.log(clicked.dataset.tab);
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add('operations__content--active');
 });
 
 //
