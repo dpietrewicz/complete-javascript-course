bad49ca286f2a0ac4272ad12ea8bca9483268c4b
Damian <d.pietrewicz4@gmail.com>
2024-04-29 13:30:59 +0200

    224. Encapsulation

--- OOP/script.js
+++ OOP/script.js
@@ -181,8 +181,8 @@ sarah.init("Sarah", 1979);
 sarah.calcAge();
 */
 
-// 219. Inheritance between classes - Constructor Funtions
-
+// 219. Inheritance between classes - Constructor Funtions -------------
+/*
 const Person = function (firstName, birthYear) {
     this.firstName = firstName;
     this.birthYear = birthYear;
@@ -218,3 +218,153 @@ console.log(mike instanceof Object);
 
 Student.prototype.constructor = Student;
 console.dir(Student.prototype.constructor);
+*/
+// 221. Inheritance between classes - ES6 -------------
+/*
+class PersonCl {
+    constructor(fullName, birthYear) {
+        this.fullName = fullName;
+        this.birthYear = birthYear;
+    }
+
+    //Instance methods
+    // Methods will be added to .prototype property
+    calcAge() {
+        console.log(2037 - this.birthYear);
+    }
+
+    greet = function () {
+        console.log(`Hey ${this.firstName}`);
+        console.log(this);
+    };
+
+    get age() {
+        return 2037 - this.birthYear;
+    }
+
+    // Set a property that already exists
+    // if this kind of "set" is created, then "get" is needed
+    set fullName(name) {
+        if (name.includes(" ")) this._fullName = name;
+        else alert(`${name} is not a full name!`);
+    }
+
+    get fullName() {
+        return this._fullName;
+    }
+}
+
+class StudentCl extends PersonCl {
+    // if no new properties needed then no constructor is needed.
+    constructor(fullName, birthYear, course) {
+        // Always needs to happen first - the super!
+        super(fullName, birthYear, course);
+        this.course = course;
+    }
+
+    introduce() {
+        console.log(`My name is ${this.fullName} and I study ${this.course}`);
+    }
+
+    calcAge() {
+        console.log(
+            `I am ${
+                2037 - this.birthYear
+            } years old, but as a student I feel more like ${
+                2037 - this.birthYear + 10
+            }`
+        );
+    }
+}
+
+const martha = new StudentCl("Martha Jones", 2012, "Computer Science");
+// const martha = new StudentCl("MArtha Jones", 2012, "Computer Science");
+martha.introduce();
+martha.calcAge();
+*/
+// 222. Inheritance between classes - Object.create
+/*
+const PersonProto = {
+    calcAge() {
+        console.log(2037 - this.birthYear);
+    },
+
+    init(firstName, birthYear) {
+        this.firstName = firstName;
+        this.birthYear = birthYear;
+    },
+};
+
+const steven = Object.create(PersonProto);
+
+const StudentProto = Object.create(PersonProto);
+
+// Adding/Overwriting methods
+StudentProto.init = function (firstName, birthYear, course) {
+    PersonProto.init.call(this, firstName, birthYear);
+    this.course = course;
+};
+
+StudentProto.introduce = function () {
+    console.log(`My name is ${this.fullName} and I study ${this.course}`);
+};
+
+const jay = Object.create(StudentProto);
+jay.init("Jay", 2010, "Computer Science");
+*/
+
+// 223. Another Class Example ---------------
+
+class Account {
+    constructor(owner, currency, pin) {
+        this.owner = owner;
+        this.currency = currency;
+        this.pin = pin;
+        // protected property
+        this._movements = [];
+        this.locale = navigator.language;
+
+        console.log(`Thanks for opening an account, ${owner}`);
+    }
+
+    // Puiblic interface
+    getMovements() {
+        return this._movements;
+    }
+
+    deposit(val) {
+        this._movements.push(val);
+    }
+
+    wirthdraw(val) {
+        this.deposit(-val);
+    }
+
+    _approveLoan(val) {
+        return true;
+    }
+
+    requestLoan(val) {
+        if (this._approveLoan(val)) {
+            this.deposit(val);
+            console.log(`Loan approved`);
+        }
+    }
+}
+
+const acc1 = new Account("Jonas", "EUR", 1111);
+console.log(acc1);
+
+// acc1._movements.push(250);
+// acc1._movements.push(-140);
+
+acc1.deposit(250);
+acc1.wirthdraw(140);
+acc1.requestLoan(1000);
+acc1._approveLoan(1000);
+console.log(acc1.getMovements());
+
+console.log(acc1);
+console.log(acc1.pin);
+
+// 225.  Encapsulation ---------------
